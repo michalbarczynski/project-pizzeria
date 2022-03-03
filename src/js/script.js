1e9fb460fb8589691788edc7056111444a4747cf
Michal Barczynski <barczynski_sggw@wp.pl>
2022-03-04 00:10:25 +0100

    task 7.4 done

--- src/js/script.js
+++ src/js/script.js
@@ -59,9 +59,9 @@
       thisProduct.id = id;
       thisProduct.data = data;
       
-      thisProduct.initAccordion();
       thisProduct.renderInMenu();
       thisProduct.getElements();
+      thisProduct.initAccordion();
 
       console.log('new Product:', thisProduct);
     }
@@ -69,7 +69,7 @@
     renderInMenu(){
       const thisProduct = this;
 
-      const generatedHTML = templates.menuProduct(thisProduct.data); //WTF?
+      const generatedHTML = templates.menuProduct(thisProduct.data); //do wyjaśnienia
 
       thisProduct.element = utils.createDOMFromHTML(generatedHTML);
 
@@ -82,37 +82,27 @@
     
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable); 
       //Tu zmienić coś w powyższej linii na thisProduct.accordionTrigger.addEventListener('click', function(event) {...}
-
-      console.log(thisProduct.accordionTrigger);
-      
       thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
-      console.log(thisProduct.form);
-
       thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs);
-      console.log(thisProduct.formInputs);
-      
       thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton);
-    
       thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem);
     }
  
    
     initAccordion(){
       const thisProduct = this;
-      console.log('dokończ funckję z CLICKABLETRIGGER');
-
       /* find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = select.querySelector(select.menuProduct.clickable); //select?document?article?
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       console.log(clickableTrigger);
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function(event) {
         /* prevent default action for event */
         event.preventDefault();
         /* find active product (product that has active class) */
-        thisProduct.activeProduct = thisProduct.element.querySelector('classNames.menuProduct.wrapperActive'); 
+        const activeProduct = document.querySelector(select.all.menuProductsActive); 
         /* if there is active product and it's not thisProduct.element, remove class active from it */
-        if (thisProduct.activeProduct != null) {
-          thisProduct.activeProduct.classList.remove('active');
+        if (activeProduct && activeProduct != thisProduct.element) {
+          activeProduct.classList.remove('active');
         }
         /* toggle active class on thisProduct.element */
         (thisProduct.element).classList.toggle('active');
@@ -172,3 +162,34 @@
 
   app.init();
 }
+
+/*
+processOrder(){
+  console.log('dokończ funckję PROCESSORDER');
+  const thisProduct = this;
+
+  // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+  const formData = utils.serializeFormToObject(thisProduct.form);
+  console.log('formData', formData);
+
+  // set price to default price
+  let price = thisProduct.data.price;
+
+  // for every category (param)...
+  for(let paramId in thisProduct.data.params) {
+    // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+    const param = thisProduct.data.params[paramId];
+    console.log(paramId, param);
+
+    // for every option in this category
+    for(let optionId in param.options) {
+      // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+      const option = param.options[optionId];
+      console.log(optionId, option);
+    }
+  }
+
+  // update calculated price in the HTML
+  thisProduct.priceElem.innerHTML = price;
+}
+*/
